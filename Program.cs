d759b94f5e830d50c6107456fdf914c55d3a40a9
Nate Faulds <nb.faulds@gmail.com>
2023-01-20 10:55:11 -0700

    Added restart/reload code on push attempt

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using BalenaNebraUpdater.Core;
 using BalenaNebraUpdater.Tools;
 using BalenaNebraUpdater.Tracking;
+using System.Diagnostics;
+
 Console.CancelKeyPress += delegate {
     new LoadingUnloading().Save();
     Environment.Exit(0); // User canceled program
@@ -15,7 +17,6 @@ if(args[0] == "true") StaticDebugger.CurrentlyDebugging = true;
 }
 // When console menu is made a menu for picking the fleet name from API will also be added so this is commented for that process
 //bel.GetFleets("key");
-
 
 
 if (!ld.Loaded()) { // No settings - prompt for initial config
@@ -50,8 +51,10 @@ while (true) {
                 SettingsStatic.Settings.CurrentCommit = currentCommit;
                 try { ld.Save(); } catch { Console.WriteLine($"{DateTime.Now} | Failed to save settings after commit update - please report this as an issue on Github"); }
                 Discord.SendWebhook();
+                ReloadMyself();
             }
-            else Console.WriteLine($"{DateTime.Now} | Commit update push failed - please report this as an issue on Github");
+            else Console.WriteLine($"{DateTime.Now} | Commit update push failed - please report this as an issue on Github"); ReloadMyself();
+                                                                                    // We'll reload ourself on failure to see if maybe the token just went stale anyway
         }
         else
         {            
@@ -63,13 +66,15 @@ while (true) {
                     SettingsStatic.Settings.CurrentCommit = currentCommit;
                     try { ld.Save(); } catch { Console.WriteLine($"{DateTime.Now} | Failed to save settings after initial commit update - please report this as an issue on Github"); }
                     Discord.SendWebhook();
+                    ReloadMyself();
                 }
-                else Console.WriteLine($"{DateTime.Now} | Initial build failed - please report this as an issue on Github");
-            }
+                else Console.WriteLine($"{DateTime.Now} | Initial build failed - please report this as an issue on Github"); ReloadMyself();
+            }                                                                       // We'll reload ourself on failure to see if maybe the token just went stale anyway
             else Console.WriteLine($"{DateTime.Now} | Update not needed");
 
         }
-    } catch(Exception ex) { Console.WriteLine("FATAL ERROR! Please report an issue to github | Failed logic loop"); Logger.SaveEntry($"Logic loop failed | {ex.Message}",BalenaNebraUpdater.Objs.Enums.ErrorLevel.Fatal); }    
+    } catch(Exception ex) { Console.WriteLine("FATAL ERROR! Please report an issue to github | Failed logic loop"); Logger.SaveEntry($"Logic loop failed | {ex.Message}",BalenaNebraUpdater.Objs.Enums.ErrorLevel.Fatal); ReloadMyself(); }
+                                                                                    // We'll reload ourself on failure to see if maybe the token just went stale anyway
 
     // Add console menu options to use the balenacli option, this code would then be used. Api is just better for easability
     //if (git.NeedsCloned())
@@ -88,4 +93,15 @@ while (true) {
     //}
     //if (!updated) { Console.WriteLine($"{DateTime.Now} | Update not needed"); }
     Thread.Sleep(3600000); // hard coded hour wait for now
+}
+
+static void ReloadMyself()
+{
+    // This is a quick workaround for the token issues with Balena push after an initial fleet update, restart the app so the token is valid next update push
+    // Whenever I get time (if I get time) to get another capture of multi update pushes to balena I'll reverse it and see what changes for the second push and update this proj
+    try {
+        Process.Start(Environment.ProcessPath);
+        Environment.Exit(4); // We'll use exit code 4 to mark that this is basically restarting a new process
+    }
+    catch(Exception ex) { Console.WriteLine("Error occured trying to start ourself after image push"); Console.WriteLine(ex.Message); }
 }
